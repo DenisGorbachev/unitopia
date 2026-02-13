a1de4916ef00e3c5a026d41a1dec5cd218755933
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2026-02-14 06:55:45 +0700

    fix: upscale

--- packages/unitopia-draft-scale/src/upscale.rs
+++ packages/unitopia-draft-scale/src/upscale.rs
@@ -3,15 +3,49 @@ pub trait Upscale<M> {
     type Output;
 
     /// The implementation must contain a `debug_assert!(numerator > denominator)`, then it can be sure that certain operations will be infallible
+    /// The implementation must contain a `debug_assert_ne!(denominator, 0);`
     /// The implementation must return a remainder
     fn upscale(self, numerator: M, denominator: M) -> Self::Output;
 }
 
 impl Upscale<u64> for u64 {
-    // TODO
-    type Output = ();
+    /// `(upscaled, remainder)` where:
+    /// `self * denominator = upscaled * numerator + remainder`.
+    ///
+    /// Returning `remainder` preserves the fractional part that would
+    /// otherwise be lost in integer-only upscaling.
+    type Output = (u64, u64);
 
-    fn upscale(self, _numerator: u64, _denominator: u64) -> Self::Output {
-        todo!()
+    fn upscale(self, numerator: u64, denominator: u64) -> Self::Output {
+        debug_assert!(numerator > denominator);
+        debug_assert_ne!(denominator, 0);
+
+        let scaled = u128::from(self)
+            .checked_mul(u128::from(denominator))
+            .expect("always succeeds because product of two u64 values always fits into u128");
+
+        let upscaled_u128 = scaled
+            .checked_div(u128::from(numerator))
+            .expect("always succeeds because numerator is greater than denominator, therefore numerator is non-zero");
+
+        let remainder_u128 = scaled
+            .checked_rem(u128::from(numerator))
+            .expect("always succeeds because numerator is greater than denominator, therefore numerator is non-zero");
+
+        let upscaled = u64::try_from(upscaled_u128).expect("always succeeds because upscaling to a larger unit with positive factors cannot increase the original u64 value");
+
+        let remainder = u64::try_from(remainder_u128).expect("always succeeds because remainder is strictly less than numerator and numerator has u64 type");
+
+        (upscaled, remainder)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Upscale;
+
+    #[test]
+    fn must_upscale_u64() {
+        assert_eq!(1550u64.upscale(1000, 1), (1, 550))
     }
 }
